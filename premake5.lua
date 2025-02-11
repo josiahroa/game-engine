e6945b3b18a6d53a004a040d4646acfa83f07869
Josiah Roa <siahroa25@gmail.com>
2025-02-10 23:23:47 -0700

    Add premake lua script to generate project files
    Added a script that can generate project files using premake. Removed any files that were previously generated by Visual Code.

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,6 @@
 workspace "Onyx"
     architecture "x64"
+    startproject "Sandbox"
 
     configurations
     {
@@ -8,13 +9,105 @@ workspace "Onyx"
         "Dist"
     }
 
-outputdir = ""
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 project "Onyx"
     location "Onyx"
     kind "SharedLib"
     language "C++"
 
-    targetDir ("bin/")
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("obj/" .. outputdir .. "/${prj.name}")
 
-ONYX_PLATFORM_WINDOWS;ONYX_BUILD_DLL
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "10.0"
+        
+        defines {
+            "ONYX_PLATFORM_WINDOWS", 
+            "ONYX_BUILD_DLL",
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+
+        buildoptions "/utf-8"
+    
+    filter "configurations:Debug"
+        defines "ONYX_DEBUG"
+        symbols "On"
+    
+    filter "configurations:Release"
+        defines "ONYX_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "ONYX_DIST"
+        optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("obj/" .. outputdir .. "/${prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+
+    includedirs
+    {
+        "Onyx/vendor/spdlog/include",
+        "Onyx/src"
+    }
+
+    links
+    {
+        "Onyx"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "10.0"
+        
+        defines {
+            "ONYX_PLATFORM_WINDOWS", 
+        }
+
+        buildoptions "/utf-8"
+    
+    filter "configurations:Debug"
+        defines "ONYX_DEBUG"
+        symbols "On"
+    
+    filter "configurations:Release"
+        defines "ONYX_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "ONYX_DIST"
+        optimize "On"
+
+    
+
+
+    
